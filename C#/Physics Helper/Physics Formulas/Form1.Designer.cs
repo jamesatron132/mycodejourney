7c5f2a3007152862fe3157f8a1436f9bed682c62
jamesatron132 <jamesatron132@gmail.com>
2017-04-09 17:44:50 +1000

    Add files via upload

---
+++ C#/Physics Helper/Physics Formulas/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Physics_Formulas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.formulaInput = new System.Windows.Forms.TextBox();
+            this.formulaOut = new System.Windows.Forms.TextBox();
+            this.launchCalc = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search the term you wish to view (all terms in lower case):";
+            // 
+            // formulaInput
+            // 
+            this.formulaInput.Location = new System.Drawing.Point(53, 53);
+            this.formulaInput.Name = "formulaInput";
+            this.formulaInput.Size = new System.Drawing.Size(177, 20);
+            this.formulaInput.TabIndex = 1;
+            this.formulaInput.TextChanged += new System.EventHandler(this.formulaInput_TextChanged);
+            // 
+            // formulaOut
+            // 
+            this.formulaOut.Location = new System.Drawing.Point(53, 107);
+            this.formulaOut.Multiline = true;
+            this.formulaOut.Name = "formulaOut";
+            this.formulaOut.Size = new System.Drawing.Size(180, 120);
+            this.formulaOut.TabIndex = 2;
+            this.formulaOut.TextChanged += new System.EventHandler(this.formulaOut_TextChanged);
+            // 
+            // launchCalc
+            // 
+            this.launchCalc.Location = new System.Drawing.Point(105, 233);
+            this.launchCalc.Name = "launchCalc";
+            this.launchCalc.Size = new System.Drawing.Size(75, 23);
+            this.launchCalc.TabIndex = 3;
+            this.launchCalc.Text = "Calculator";
+            this.launchCalc.UseVisualStyleBackColor = true;
+            this.launchCalc.Click += new System.EventHandler(this.launchCalc_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Formula/Definition below:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "e.g. force, velocity, newton\'s second law";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.launchCalc);
+            this.Controls.Add(this.formulaOut);
+            this.Controls.Add(this.formulaInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Physics Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox formulaInput;
+        private System.Windows.Forms.TextBox formulaOut;
+        private System.Windows.Forms.Button launchCalc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ C#/Physics Helper/Physics Formulas/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Physics_Formulas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void formulaOut_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void formulaInput_TextChanged(object sender, EventArgs e)
+        {
+            if (formulaInput.Text == "force" || formulaInput.Text == "newton's second law")
+            {
+                formulaOut.Text = @"F=ma 
+a force is any interaction that, when unopposed, will change the motion of an object.";
+            }
+            else if (formulaInput.Text == "velocity")
+            {
+                formulaOut.Text = @"v=r/t
+The speed of something in a given direction.";
+            }
+            else if (formulaInput.Text == "acceleration")
+            {
+                formulaOut.Text = @"a=v-u/t
+a=F/m
+Acceleration is a vector quantity that is defined as the rate at which an object changes its velocity. An object is accelerating if it is changing its velocity.";
+            }
+            else if (formulaInput.Text == "displacement")
+            {
+                formulaOut.Text = @"Displacement is defined as r.
+Distance is a scalar quantity that refers to how much ground an object has covered during its motion. Displacement is a vector quantity that refers to, how far out of place an object is. It is the object's overall change in position.";
+            }
+            else if (formulaInput.Text == "mass")
+            {
+                formulaOut.Text = @"m=F/a
+In physics, mass is a property of a physical body. It is the measure of an object's resistance to acceleration (a change in its state of motion) when a net force is applied. It also determines the strength of its mutual gravitational attraction to other bodies.";
+            }
+            else if (formulaInput.Text == "newton's first law" || formulaInput.Text == "law of inertia")
+            {
+                formulaOut.Text = @"An object at rest stays at rest and an object in motion stays in motion with the same speed and in the same direction unless acted upon by an unbalanced force.";
+            }
+            else if (formulaInput.Text == "newton's third law")
+            {
+                formulaOut.Text = @"For every action, there is an equal and opposite reaction.";
+            }
+            else
+            {
+                formulaOut.Text = "";
+            }
+        }
+
+        private void launchCalc_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("calc.exe");
+        }
+    }
+}
